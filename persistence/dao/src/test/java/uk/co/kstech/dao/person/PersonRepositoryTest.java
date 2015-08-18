7d85b99f2f3d990b4897814d09993b9d52b45fb4
Kieran McGivern <kmcgivern@navinet.net>
2015-08-18 14:41:44 +0100

    minor test change

--- persistence/dao/src/test/java/uk/co/kstech/dao/person/PersonRepositoryTest.java
+++ persistence/dao/src/test/java/uk/co/kstech/dao/person/PersonRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 import uk.co.kstech.dao.TestJpaConfig;
 import uk.co.kstech.model.person.Person;
 
@@ -21,6 +20,8 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import static org.junit.Assert.assertThat;
+import static org.springframework.util.Assert.notNull;
+
 
 /**
  * Created by KMcGivern on 7/17/2014.
@@ -52,25 +53,37 @@ public class PersonRepositoryTest {
     @Test
     public void shouldSavePerson() throws Exception {
         final Person saved = savePerson();
-        Assert.notNull(saved.getId());
+        notNull(saved.getId());
     }
 
     @Test
     public void shouldSaveRelationship() throws Exception {
         final Person child = classUnderTest.save(createChild());
         final Person father = classUnderTest.save(createFather(child));
-        Assert.notNull(child.getId());
-        Assert.notNull(father.getId());
-        Assert.notNull(father.getChildren().iterator().next().getId());
+        notNull(child.getId());
+        notNull(father.getId());
+        notNull(father.getChildren().iterator().next().getId());
+        assertThat(father.getChildren().iterator().next().getId(), IsEqual.equalTo(child.getId()));
     }
 
     @Test
     public void shouldRetrievePerson() {
+        //arrange
         final Person saved = savePerson();
+        //act
         final Person loadedPerson = classUnderTest.findOne(saved.getId());
-        Assert.notNull(loadedPerson.getId());
+        //assert
+        notNull(loadedPerson.getId());
     }
 
+    @Test
+    public void shouldGetAllChildrenForParent(){
+        final Person father = classUnderTest.save(createFather());
+        father.addChild(createChild("John", "Peter", father.getLastName(), LocalDate.of(2000, Month.NOVEMBER, 20), true));
+        father.addChild(createChild("James", "Paul", father.getLastName(), LocalDate.of(2002, Month.JANUARY, 15), true));
+        classUnderTest.save(father);
+        assertThat(father.getChildren().size(), IsEqual.equalTo(2));
+    }
 
     private Person savePerson() {
         final Person saved = classUnderTest.save(createFather());
@@ -84,7 +97,6 @@ public class PersonRepositoryTest {
         Person.setLastName("Davids");
         Person.setMale(true);
         Person.setBirthDate(LocalDate.of(1980, Month.JANUARY, 20));
-        Person.addChild(createChild());
         return Person;
     }
 
@@ -107,6 +119,17 @@ public class PersonRepositoryTest {
         child.setLastName("Davids");
         child.setMale(true);
         child.setBirthDate(LocalDate.of(2000, Month.NOVEMBER, 20));
+        return child;
+    }
+
+    private Person createChild(final String first, final String middle, final String last, final LocalDate dob, final boolean isMale) {
+
+        Person child = new Person();
+        child.setFirstName(first);
+        child.setMiddleName(middle);
+        child.setLastName(last);
+        child.setMale(isMale);
+        child.setBirthDate(dob);
         return child;
     }
 
